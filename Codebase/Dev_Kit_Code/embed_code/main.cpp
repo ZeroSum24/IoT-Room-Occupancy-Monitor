1cedbfbc8c9e1807ca9ae2eea82ac8d369cd97e0
Samuel Knight <knight.samuel@outlook.com>
2019-02-03 17:18:10 +0000

    Added bluetooth functions. Included calls to initialize bluetooth in main file.

--- Codebase/Dev_Kit_Code/embed_code/main.cpp
+++ Codebase/Dev_Kit_Code/embed_code/main.cpp
@@ -1,11 +1,21 @@
 #include "mbed.h"
+#include "bluetooth.h"
 
 DigitalOut led1(LED1);
-DigitalIn alarm(p29, PullNone); //internal pull up 
+DigitalIn alarm(p29, PullNone); //internal pull up
 
-int main() {  
+int main() {
     wait(2); //Wait for sensor to take snap shot of still room
-    
+
+    // create BLE instance
+    BLE &ble = BLE::Instance();
+    // link to events processing handler
+    ble.onEventsToProcess(scheduleBleEventsProcessing);
+    // initialise the BLE device
+    ble.init(bleInitComplete);
+
+    eventQueue.dispatch_forever();
+
     while(1) {
         if (!alarm){
             led1=1;
