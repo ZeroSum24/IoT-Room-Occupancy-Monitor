9f4d029ad545252baeeb638156a2fd5b1c1231cd
Samuel Knight <knight.samuel@outlook.com>
2019-02-07 15:33:31 +0000

    Revert "Some updated bluetooth and main integration"
    This reverts commit e87033c6f78bafbfbc261f3001712628fa627c82.

--- Codebase/Dev_Kit_Code/embed_code/bluetooth.cpp
+++ Codebase/Dev_Kit_Code/embed_code/bluetooth.cpp
@@ -3,12 +3,12 @@
 #include "ble/BLE.h"
 #include "ble/Gap.h"
 #include "ble/services/HeartRateService.h"
-#include "bluetooth.h"
 
-char DEVICE_NAME[] = "SONICONE";
-EventQueue eventQueue(/* event count */ 16 * EVENTS_EVENT_SIZE);
-uint8_t hrmCounter = 100;
-HeartRateService *hrServicePtr;
+const static char DEVICE_NAME[] = "WOW";
+
+static EventQueue eventQueue(/* event count */ 16 * EVENTS_EVENT_SIZE);
+static uint8_t hrmCounter = 100;
+static HeartRateService *hrServicePtr;
 
 
 void onBleInitError(BLE &ble, ble_error_t error)
--- Codebase/Dev_Kit_Code/embed_code/main.cpp
+++ Codebase/Dev_Kit_Code/embed_code/main.cpp
@@ -1,25 +1,8 @@
-#include <events/mbed_events.h>
-#include <mbed.h>
-#include "ble/BLE.h"
-#include "ble/Gap.h"
-#include "ble/services/HeartRateService.h"
+#include "mbed.h"
 #include "bluetooth.h"
 
 DigitalOut led1(LED1);
-//DigitalOut led2(LED2);
-DigitalIn alarm1(p29, PullNone); //internal pull up
-//DigitalIn alarm2(p28, PullNone);
-
-void checkAlarm(DigitalIn& alarm, DigitalOut& led_to_trigger)
-{
-    if (!alarm){
-        led_to_trigger=1;
-        wait(2);
-    }
-    else
-        led_to_trigger=0;
-}
-
+DigitalIn alarm(p29, PullNone); //internal pull up
 
 int main() {
     wait(2); //Wait for sensor to take snap shot of still room
@@ -27,13 +10,18 @@ int main() {
     // create BLE instance
     BLE &ble = BLE::Instance();
     // link to events processing handler
-    //ble.onEventsToProcess(scheduleBleEventsProcessing);
+    ble.onEventsToProcess(scheduleBleEventsProcessing);
     // initialise the BLE device
-    //ble.init(bleInitComplete);
+    ble.init(bleInitComplete);
 
-    //eventQueue.dispatch_forever();
+    eventQueue.dispatch_forever();
 
-    //while(1) {
-        //checkAlarm(alarm1, led1);
-    //}
+    while(1) {
+        if (!alarm){
+            led1=1;
+            wait(2);
+        }
+        else
+            led1=0;
+    }
 }
