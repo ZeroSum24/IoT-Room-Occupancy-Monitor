fde7a99ebd135432bda07ee9fd36737130ba7106
Samuel Knight <knight.samuel@outlook.com>
2019-02-07 15:33:31 +0000

    Revert "Some updated bluetooth and main integration"
    This reverts commit e87033c6f78bafbfbc261f3001712628fa627c82.

--- Codebase/Dev_Kit_Code/embed_code/bluetooth.h
+++ Codebase/Dev_Kit_Code/embed_code/bluetooth.h
@@ -1,8 +1,3 @@
-extern char DEVICE_NAME[];
-extern EventQueue eventQueue;
-extern uint8_t hrmCounter;
-extern HeartRateService *hrServicePtr;
-
 void onBleInitError(BLE &ble, ble_error_t error);
 
 void bleInitComplete(BLE::InitializationCompleteCallbackContext *params);
