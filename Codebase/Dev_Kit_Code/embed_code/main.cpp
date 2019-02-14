19aee797226cc2e235eee2dfec3f7d65d3a0a3e0
Samuel Knight <knight.samuel@outlook.com>
2019-02-14 18:13:56 +0000

    Added bluetooth code for a custom characteristic. Added main code to advertise bluetooth and update readable characteristic when PIR activated.

--- Codebase/Dev_Kit_Code/embed_code/main.cpp
+++ Codebase/Dev_Kit_Code/embed_code/main.cpp
@@ -1,8 +1,12 @@
-#include "mbed.h"
+#include <events/mbed_events.h>
+#include <mbed.h>
+#include "ble/BLE.h"
+#include "ble/Gap.h"
+#include "ble/services/HeartRateService.h"
 #include "bluetooth.h"
 
 DigitalOut led1(LED1);
-DigitalIn alarm(p29, PullNone); //internal pull up
+DigitalIn alarm(p28, PullDown); //internal pull up
 
 int main() {
     wait(2); //Wait for sensor to take snap shot of still room
@@ -14,14 +18,16 @@ int main() {
     // initialise the BLE device
     ble.init(bleInitComplete);
 
-    eventQueue.dispatch_forever();
-
     while(1) {
-        if (!alarm){
-            led1=1;
-            wait(2);
-        }
-        else
-            led1=0;
+       if (!alarm){
+           led1=1;
+           pirTriggered(true);
+           wait(2);
+       }
+       else {
+           led1=0;
+           pirTriggered(false);
+       }
+       eventQueue.dispatch(10);
     }
 }
