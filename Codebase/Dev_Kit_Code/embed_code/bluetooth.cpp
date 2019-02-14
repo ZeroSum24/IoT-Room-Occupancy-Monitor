90820ea5f1b1642362150b37c452316bc6e0fcf4
Samuel Knight <knight.samuel@outlook.com>
2019-02-14 18:13:56 +0000

    Added bluetooth code for a custom characteristic. Added main code to advertise bluetooth and update readable characteristic when PIR activated.

--- Codebase/Dev_Kit_Code/embed_code/bluetooth.cpp
+++ Codebase/Dev_Kit_Code/embed_code/bluetooth.cpp
@@ -3,13 +3,44 @@
 #include "ble/BLE.h"
 #include "ble/Gap.h"
 #include "ble/services/HeartRateService.h"
+#include "bluetooth.h"
 
-const static char DEVICE_NAME[] = "WOW";
+char DEVICE_NAME[] = "sonicone";
+EventQueue eventQueue(/* event count */ 16 * EVENTS_EVENT_SIZE);
+uint16_t customServiceUUID  = 0xA000;
+uint16_t readPIRUUID       = 0xA001;
+static const uint16_t uuid16_list[]        = {0xFFFF};
 
-static EventQueue eventQueue(/* event count */ 16 * EVENTS_EVENT_SIZE);
-static uint8_t hrmCounter = 100;
-static HeartRateService *hrServicePtr;
+// Set Up custom Characteristics
+static uint8_t readValue[10] = {0};
+ReadOnlyArrayGattCharacteristic<uint8_t, sizeof(readValue)> readPIR(
+  readPIRUUID, readValue
+);
 
+GattCharacteristic *characteristics[] = {&readPIR};
+GattService        customService(customServiceUUID, characteristics, sizeof(characteristics) / sizeof(GattCharacteristic *));
+
+void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *)
+{
+    BLE::Instance(BLE::DEFAULT_INSTANCE).gap().startAdvertising();
+}
+
+void pirTriggered(bool triggered)
+{
+  if (triggered)
+  {
+    uint8_t new_value[10] = {1};
+    BLE::Instance(BLE::DEFAULT_INSTANCE).gattServer().write(
+      readPIR.getValueHandle(), new_value, sizeof(new_value)
+    );
+    return;
+  }
+  uint8_t new_value[10] = {0};
+  BLE::Instance(BLE::DEFAULT_INSTANCE).gattServer().write(
+    readPIR.getValueHandle(), new_value, sizeof(new_value)
+  );
+
+}
 
 void onBleInitError(BLE &ble, ble_error_t error)
 {
@@ -18,6 +49,8 @@ void onBleInitError(BLE &ble, ble_error_t error)
    /* Initialization error handling should go here */
    // TODO: Add error logic
 }
+
+
 
 // Initiialization routine goes here
 void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
@@ -36,12 +69,16 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
         return;
     }
 
-    hrServicePtr = new HeartRateService(ble, hrmCounter, HeartRateService::LOCATION_FINGER);
+    ble.gap().onDisconnection(disconnectionCallback);
 
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::GENERIC_HEART_RATE_SENSOR);
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list)); // UUID's broadcast in advertising packet
     ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
     ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
     ble.gap().setAdvertisingInterval(1000);
+
+    // Add custom service
+    ble.addService(customService);
+
     ble.gap().startAdvertising();
 }
 
--- Codebase/Dev_Kit_Code/embed_code/bluetooth.h
+++ Codebase/Dev_Kit_Code/embed_code/bluetooth.h
@@ -1,5 +1,12 @@
+extern char DEVICE_NAME[];
+extern EventQueue eventQueue;
+extern uint8_t hrmCounter;
+extern HeartRateService *hrServicePtr;
+
 void onBleInitError(BLE &ble, ble_error_t error);
 
 void bleInitComplete(BLE::InitializationCompleteCallbackContext *params);
 
 void scheduleBleEventsProcessing(BLE::OnEventsToProcessCallbackContext* context);
+
+void pirTriggered(bool triggered);
