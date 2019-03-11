515a22e49508912db987f15737ad49e34173b443
zerosum24 <stephen_waddell@hotmail.co.uk>
2019-03-11 14:38:56 +0000

    Firebase Authentication added

--- Codebase/App_Code/IOT_App/app/src/main/java/sonicwaves/android/iot_app/ApplicationData.java
+++ Codebase/App_Code/IOT_App/app/src/main/java/sonicwaves/android/iot_app/ApplicationData.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import java.util.List;
 
 import sonicwaves.android.iot_app.adapter.DiscoveredBluetoothDevice;
+import sonicwaves.android.iot_app.firebase.FirebaseUtils;
 
 
 public class ApplicationData extends Application {
--- Codebase/App_Code/IOT_App/app/src/main/java/sonicwaves/android/iot_app/firebase/FirebaseUtils.java
+++ Codebase/App_Code/IOT_App/app/src/main/java/sonicwaves/android/iot_app/firebase/FirebaseUtils.java
@@ -1,16 +1,21 @@
 package sonicwaves.android.iot_app.firebase;
 
+import android.content.Context;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import androidx.annotation.NonNull;
+import sonicwaves.android.iot_app.R;
 import sonicwaves.android.iot_app.firebase.objects.chair.Chair;
 import sonicwaves.android.iot_app.firebase.objects.chair.ChairDetection;
 import sonicwaves.android.iot_app.firebase.objects.door.DoorDetection;
@@ -28,8 +33,67 @@ public class FirebaseUtils {
     private FirebaseFirestore db;
 
 
-    public FirebaseUtils() {
+    public FirebaseUtils(Context context) {
+
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setApplicationId(context.getResources().getString(R.string.firebase_application_id)) // Required for Analytics.
+                .setApiKey(context.getResources().getString(R.string.firebase_api_key)) // Required for Auth.
+                .setProjectId(context.getResources().getString(R.string.firebase_project_id))
+                .build();
+        FirebaseApp.initializeApp(context /* Context */, options, "secondary");
+
         db = FirebaseFirestore.getInstance();
+    }
+
+    public void testDb() {
+        Log.d(TAG, "Test Database method");
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    public void testDb2() {
+        // Create a new user with a first, middle, and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Alan");
+        user.put("middle", "Mathison");
+        user.put("last", "Turing");
+        user.put("born", 1912);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 
     public void updateChairData(Chair[] chairs) {
