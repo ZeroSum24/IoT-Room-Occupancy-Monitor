eadc0cc8f7bf72ed79746c4013a140e5335171fe
zerosum24 <stephen_waddell@hotmail.co.uk>
2019-03-11 15:40:08 +0000

    Firebase functional with Login activity added

--- Codebase/App_Code/IOT_App/app/src/main/java/sonicwaves/android/iot_app/ApplicationData.java
+++ Codebase/App_Code/IOT_App/app/src/main/java/sonicwaves/android/iot_app/ApplicationData.java
@@ -1,5 +1,6 @@
 package sonicwaves.android.iot_app;
 import android.app.Application;
+import android.content.Context;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ import sonicwaves.android.iot_app.firebase.FirebaseUtils;
 
 public class ApplicationData extends Application {
     private List<DiscoveredBluetoothDevice> selectedDevices;
+    private FirebaseUtils firebaseUtils;
 
     public List<DiscoveredBluetoothDevice> getDevices() {
         return selectedDevices;
@@ -16,5 +18,13 @@ public class ApplicationData extends Application {
 
     public void setDevices(List<DiscoveredBluetoothDevice> devices) {
         this.selectedDevices = devices;
+    }
+
+    public FirebaseUtils getFirebaseUtils(Context context) {
+
+        if (firebaseUtils == null) {
+            firebaseUtils = new FirebaseUtils(context);
+        }
+        return firebaseUtils;
     }
 }
