632c9c1d3380e986cf5585d697db2c63dddbe985
zerosum24 <stephen_waddell@hotmail.co.uk>
2019-03-15 14:45:21 +0000

    Material dashboard added

---
+++ Codebase/Dashboard/material-dashboard-react/src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+// core components
+import Admin from "./layouts/Admin.jsx";
+import RTL from "./layouts/RTL.jsx";
+
+import "./assets/css/material-dashboard-react.css";
+
+const hist = createBrowserHistory();
+
+// connect the app to firebase
+// var cred = credentials.Certificate('../../iot-app-3386d-firebase-adminsdk-notbj-6b009a7f9d.json')
+// firebase_admin.initialize_app(cred)
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/admin" component={Admin} />
+      <Route path="/rtl" component={RTL} />
+      <Redirect from="/" to="/admin/dashboard" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
