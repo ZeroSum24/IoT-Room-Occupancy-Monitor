f2d97d49c54fb181c7af91ebd2c4c6ec7c07e089
zerosum24 <stephen_waddell@hotmail.co.uk>
2019-03-19 10:53:46 +0000

    Occupancy Graph added

--- Codebase/Dashboard/material-dashboard-react/src/views/Icons/Icons.jsx
+++ Codebase/Dashboard/material-dashboard-react/src/views/Icons/Icons.jsx
@@ -1,67 +1,113 @@
 import React from "react";
 import PropTypes from "prop-types";
+// react plugin for creating charts
+import ChartistGraph from "react-chartist";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
-import Hidden from "@material-ui/core/Hidden";
 // core components
 import GridItem from "../../components/Grid/GridItem.jsx";
 import GridContainer from "../../components/Grid/GridContainer.jsx";
 import Card from "../../components/Card/Card.jsx";
 import CardHeader from "../../components/Card/CardHeader.jsx";
 import CardBody from "../../components/Card/CardBody.jsx";
+import Paper from "@material-ui/core/Paper";
 
 import iconsStyle from "../../assets/jss/material-dashboard-react/views/iconsStyle.jsx";
+import BarGraph from "../../components/Graphs/BarGraph";
 
-function Icons(props) {
-  const { classes } = props;
-  return (
-    <GridContainer>
-      <GridItem xs={12} sm={12} md={12}>
-        <Card plain>
-          <CardHeader plain color="primary">
-            <h4 className={classes.cardTitleWhite}>Material Design Icons</h4>
-            <p className={classes.cardCategoryWhite}>
-              Handcrafted by our friends from{" "}
-              <a
-                href="https://design.google.com/icons/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Google
-              </a>
-            </p>
-          </CardHeader>
-          <CardBody>
-            <Hidden only={["sm", "xs"]}>
-              <iframe
-                className={classes.iframe}
-                src="https://material.io/icons/"
-                title="Icons iframe"
-              >
-                <p>Your browser does not support iframes.</p>
-              </iframe>
-            </Hidden>
-            <Hidden only={["lg", "md"]}>
-              <GridItem xs={12} sm={12} md={6}>
-                <h5>
-                  The icons are visible on Desktop mode inside an iframe. Since
-                  the iframe is not working on Mobile and Tablets please visit
-                  the icons on their original page on Google. Check the
-                  <a
-                    href="https://design.google.com/icons/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Material Icons
-                  </a>
-                </h5>
-              </GridItem>
-            </Hidden>
-          </CardBody>
-        </Card>
-      </GridItem>
-    </GridContainer>
-  );
+import {
+  occupancyStatsChart,
+  spaceUsageChart,
+  roomUsageChart
+} from "../../variables/chartsDetails.jsx";
+
+const styles = {
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus": {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0"
+    },
+    "& a,& a:hover,& a:focus": {
+      color: "#FFFFFF"
+    }
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  }
+};
+
+class Icons extends React.Component  {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomOccupancyContents: "23 ",
+      chairsFreeContents: "49/50",
+      mostPopularTimeContents: "12AM",
+      totalOccupancyContents: "+234",
+      roomUsageData: [[23, 75, 45, 30, 28, 24, 20, 19]],
+      spaceUsageData: [[54, 43]],
+      occupancyStatsData: [[12, 17, 7, 17, 23, 18, 38]],
+    };
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+    // this.setState({xScale: ReactD3.time.scale().domain([extent[0], extent[1]]).range([0, 400 - 70])});
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card>
+            <CardHeader color="primary">
+              <h4 className={classes.cardTitleWhite}>Occupancy Statistics</h4>
+              <p className={classes.cardCategoryWhite}>
+                Derived from room data per hour
+              </p>
+            </CardHeader>
+            <CardBody>
+              <Paper style={{background: "#48A44C", padding:100}}>
+                <ChartistGraph
+                  className="ct-chart"
+                  data={occupancyStatsChart(this.state.occupancyStatsData).data}
+                  type="Line"
+                  options={occupancyStatsChart.options}
+                  listener={occupancyStatsChart.animation}
+                />
+              </Paper>
+            </CardBody>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={12}>
+            <Card plain>
+
+              <CardBody>
+
+              </CardBody>
+            </Card>
+        </GridItem>
+      </GridContainer>
+    );
+  }
 }
 
 Icons.propTypes = {
