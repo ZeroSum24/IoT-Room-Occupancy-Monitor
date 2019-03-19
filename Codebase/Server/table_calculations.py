0a76d05d8acb391cfacf7b35612ac18e7251f48c
Samuel Knight <knight.samuel@outlook.com>
2019-03-19 18:35:33 +0000

    table calc skeleton.

---
+++ Codebase/Server/table_calculations.py
@@ -0,0 +1,10 @@
+
+# Return appropriate visualization data
+def table_analysis(table_id, readingsList):
+    tables_to_chairs = {'table1': [0,0], 'table2': [0,0]}
+    for reading in readingsList:
+        device_timestamp = reading['device_timestamp']
+        chair_id = reading['chair_id']
+        signal_strength = signal_strength['signal_strength']
+
+    
