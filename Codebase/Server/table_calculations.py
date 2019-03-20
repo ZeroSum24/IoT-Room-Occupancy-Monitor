75b093f1394d8c6cf8a53d3f1330d0fbc093f5a5
Samuel Knight <knight.samuel@outlook.com>
2019-03-20 16:51:46 +0000

    Small update to server code and table calculations.

--- Codebase/Server/table_calculations.py
+++ Codebase/Server/table_calculations.py
@@ -1,10 +1,23 @@
 
 # Return appropriate visualization data
 def table_analysis(table_id, readingsList):
-    tables_to_chairs = {'table1': [0,0], 'table2': [0,0]}
+    registered_chairs = {'chair1': 0, 'chair2': 0, 'chair3': 0}
+    final_reading = readingsList[len(readingsList)-1]
+    
+
     for reading in readingsList:
-        device_timestamp = reading['device_timestamp']
+        
         chair_id = reading['chair_id']
-        signal_strength = signal_strength['signal_strength']
+        inverse_signal_strength = reading['signal_strength']
+        if inverse_signal_strength < 65: 
+            # note that high inverse_signal_strength means chair is further away
+            registered_chairs['chair'+str(chair_id)] = 1
+    return current_chairs, historical_chairs
+
+def set_tables(chair_dict):
+    
+        
+
+
 
     
