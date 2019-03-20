7307e02d40d1b2b9043db9d28ad250e9389ac179
zerosum24 <stephen_waddell@hotmail.co.uk>
2019-03-20 17:12:23 +0000

    Chair calculations updated

--- Codebase/Server/chair_calculations.py
+++ Codebase/Server/chair_calculations.py
@@ -2,7 +2,7 @@ from utils import calculate_time_diff
 
 # takes in chair id and a list of detections (which is a dictionary with each field value as the key)
 # assumes the readings list is ordered into true/false pairs based on the timestamp
-def chair_analysis(chair_id, readingsList):
+def chair_analysis(readingsList):
 
     currently_used = False
     templateDict = {"initialTimestamp": "", "finalTimestamp": "", "duration": 0}
@@ -19,15 +19,15 @@ def chair_analysis(chair_id, readingsList):
             chair_usage.append(templateDict)
 
             # remove the used readings
-            readingsList.remove(i)
             readingsList.remove(i+1)
+            readingsList.remove(i)
         }
         i+=1
     }
 
     # if the last item does not have a pair and it is activated then it is currently_used
-    if (len(readingsList) == 1):
-        if (readingsList[0]['activated']):
+    if (len(readingsList) >= 1):
+        if (readingsList[len(readingsList)-1]['activated']):
             currently_used = True
 
     return (currently_used, chair_usage)
