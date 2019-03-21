91ea352a69b219cfe97573bae31de1b4c123b6e9
zerosum24 <stephen_waddell@hotmail.co.uk>
2019-03-21 02:13:51 +0000

    JS Server utils and main updates

--- Codebase/Server/run_js_server.js
+++ Codebase/Server/run_js_server.js
@@ -4,7 +4,14 @@ const admin = require('firebase-admin');
 var serviceAccount = require('./iot-app-3386d-firebase-adminsdk-notbj-6b009a7f9d.json');
 
 //our imports
-var chair_cal = require('./chair_calculations')
+// var chair_cal = require('./chair_calculations')
+var utils = require('./utils')
+//
+
+
+let last_read_timestamps = {'SonicWaves-C-001': 0, 'SonicWaves-C-002':0, 'SonicWaves-C-003':0,
+                            'SonicWaves-T-001': 0, 'SonicWaves-T-002':0, 'SonicWaves-D-001': 0}
+
 // inititalise the js server
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,17 +21,11 @@ var db = admin.firestore();
 
 // functions
 
-function databaseWatcher(query, callback_function) {
+function databaseWatcher(query, callback_function, device_name) {
 
   var observer = query.onSnapshot(snapshot => {
     console.log(`Received query snapshot of size ${snapshot.size}`);
-    snapshot.docChanges().forEach(function(change) {
-            if (change.type === "added") {
-                console.log("New city: ", change.doc.data());
-                callback_function()
-            }
-            console.log(change.doc.id)
-        });
+    callback_function(snapshot, device_name);
   }, err => {
     console.log(`Encountered error: ${err}`);
   });
@@ -32,32 +33,65 @@ function databaseWatcher(query, callback_function) {
 
 // callback functions
 
-function chair_callback() {
-  console.log("chair - success");
+function chair_callback(snapshot, device_name) {
+  // converts all the updated readings into a javascript object
+  var updated_chair_readings = {}
+  snapshot.docChanges().forEach(function(change) {
+          if (change.type === "added") {
+              console.log(device_name, "chair reading: ", change.doc.id, change.doc.data());
+              updated_chair_readings[change.doc.id] = change.doc.data()
+          }
+      });
+  // filter and sort the data
+  var out_array = utils.filter_sort_data(updated_chair_readings, last_read_timestamps[device_name])
+  // updated_chair_readings = out_array[0]
+  // last_read_timestamps[device_name] = out_array[1]
+
+  // callback_function(snapshot)
 }
 
-function door_callback() {
-  console.log("door - success");
+function door_callback(snapshot, device_name) {
+  // converts all the updated readings into a javascript object
+  var updated_door_readings = {}
+  snapshot.docChanges().forEach(function(change) {
+          if (change.type === "added") {
+              console.log(device_name, "door reading: ", change.doc.id, change.doc.data());
+              updated_door_readings[change.doc.id] = change.doc.data()
+          }
+      });
+  // callback_function()
 }
 
-function table_callback() {
-  console.log("table - success");
+function table_callback(snapshot, device_name) {
+  // converts all the updated readings into a javascript object
+  var updated_table_readings = {}
+  snapshot.docChanges().forEach(function(change) {
+          if (change.type === "added") {
+              console.log(device_name, "table reading: ", change.doc.id, change.doc.data());
+              updated_table_readings[change.doc.id] = change.doc.data()
+          }
+      });
+  // callback_function()
+}
+
+function getDataVisual() {
+
 }
 
 // inititialise chair database watchers
 var chairOneQuery = db.collection('chair_data').doc('SonicWaves-C-001').collection('detections');
 var chairTwoQuery = db.collection('chair_data').doc('SonicWaves-C-002').collection('detections');
 var chairThreeQuery = db.collection('chair_data').doc('SonicWaves-C-003').collection('detections');
-databaseWatcher(chairOneQuery, chair_callback) //chair one watcher
-databaseWatcher(chairTwoQuery, chair_callback) //chair one watcher
-databaseWatcher(chairThreeQuery, chair_callback) //chair one watcher
+databaseWatcher(chairOneQuery, chair_callback, 'SonicWaves-C-001') //chair one watcher
+databaseWatcher(chairTwoQuery, chair_callback, 'SonicWaves-C-002') //chair one watcher
+databaseWatcher(chairThreeQuery, chair_callback, 'SonicWaves-C-003') //chair one watcher
 
 // inititialise door database watchers
 var doorOneQuery = db.collection('door_data').doc('SonicWaves-D-001').collection('detections');
-databaseWatcher(doorOneQuery, door_callback) //door one watcher
+databaseWatcher(doorOneQuery, door_callback, 'SonicWaves-D-001') //door one watcher
 
 // inititialise table database watchers
 var tableOneQuery = db.collection('table_data').doc('SonicWaves-T-001').collection('detections');
 var tableTwoQuery = db.collection('table_data').doc('SonicWaves-T-002').collection('detections');
-databaseWatcher(tableOneQuery, table_callback) //table one watcher
-databaseWatcher(tableTwoQuery, table_callback) //table one watcher
+databaseWatcher(tableOneQuery, table_callback, 'SonicWaves-T-001') //table one watcher
+databaseWatcher(tableTwoQuery, table_callback, 'SonicWaves-T-002') //table one watcher
---
+++ Codebase/Server/utils.js
@@ -0,0 +1,67 @@
+
+module.exports = {
+  filter_sort_data,
+}
+
+function filter_sort_data(readingsList, last_timestamp) {
+    data = []
+
+    // set the largest timestamp if still default
+    if (largest_timestamp == 0) {
+      largest_timestamp = new Date(year=datetime.MINYEAR, month=1, day=1);
+    }
+
+    var timestamp;
+    // iterate over the indexes of the dictionary
+    for (index in Object.entries(readingsList)) {
+      var values = readingsList[index]
+      timestamp = values['timestamp']
+
+      readingDate = parse_timestamp(timestamp)
+      if (readingDate >= last_timestamp) {
+          if (readingDate > largest_timestamp) {
+              largest_timestamp = readingDate;
+            }
+          data.push([doc, readingDate]);
+        }
+      }
+    data = array.sort(function(a,b) {
+      return new Date(b[1].date) - new Date(a[1].date);
+    })
+
+    // only return the first element of each 2d array
+    for (var i = 1; i <= data.length; i++) {
+      data[i] = data[i][0]
+    }
+    console.log("Working")
+    console.log([data, largest_timestamp])
+    console.log(data)
+    return [data, largest_timestamp]
+  }
+
+// parses a string into a datetime object
+function parse_timestamp(datetime_str) {
+    var [date, time] = datetime_str.split('_')
+
+    // calculate date
+    var [year, month, day] = date.split('-')
+    year = parseInt(year)
+    month = parseInt(month)
+    day = parseInt(day)
+
+    // calculate time
+    var [hour, minute, second, milli] = time.split('-')
+    hour = parseInt(hour)
+    minute = parseInt(minute)
+    second = parseInt(second)
+    milli = parseInt(milli)
+
+    //returns the date from parsing
+    return (new Date(year, month, day, hour, minute, second, milli));
+  }
+
+sortArrayOfObjects = (arr, key) => {
+  return arr.sort((a, b) => {
+      return a[key] - b[key];
+  });
+};
